cad614af9fbc77f5a49d6ec0ae9509b958f6cece
mrahman4782 <moharahman50@gmail.com>
2021-03-18 23:43:28 -0400

    Second Commit

---
+++ app/src/main/java/com/example/instagramclone/fragments/ProfileFragment.java
@@ -0,0 +1,36 @@
+package com.example.instagramclone.fragments;
+
+import com.example.instagramclone.Post;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
+
+public class ProfileFragment extends PostsFragment {
+
+    @Override
+    protected void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.whereEqualTo(Post.KEY_USER, ParseUser.getCurrentUser());
+        query.setLimit(20);
+        query.addDescendingOrder(Post.KEY_CREATED_KEY);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    return;
+                }
+                for (Post post : posts) {
+
+                }
+                allPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+}
+
+
